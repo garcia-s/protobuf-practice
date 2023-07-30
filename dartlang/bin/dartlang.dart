b5f1cc409df4642e943d096672f1dbba50ac33ee
Juan García <43949658+garcia-s@users.noreply.github.com>
2023-07-30 11:52:52 -0500

    Created project

---
+++ dartlang/bin/dartlang.dart
@@ -0,0 +1,5 @@
+import 'package:dartlang/dartlang.dart' as dartlang;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dartlang.calculate()}!');
+}
---
+++ dartlang/test/dartlang_test.dart
@@ -0,0 +1,8 @@
+import 'package:dartlang/dartlang.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
