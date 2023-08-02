baccf02610b51b339d3c1ffd88d7f0ded335e4da
Juan Garcia <garciajj18@gmail.com>
2023-08-01 19:48:21 -0500

    Moving forward

--- client/client.go
+++ client/client.go
@@ -3,13 +3,17 @@ package main
 import (
 	"bytes"
 	"fmt"
+
+	"net/http"
+
 	"github.com/garcia-s/protobuf-practice/client/proto/users"
 	"google.golang.org/protobuf/proto"
-	"net/http"
 )
 
 func main() {
-	url := "http://localhost:8080/hello"
+	url := "http://localhost:8080"
+	createRoute := "/hello"
+
 	user := users.NewUser{
 		Username: "juanito",
 		Password: "thisisapassword",
@@ -19,11 +23,20 @@ func main() {
 		panic(e)
 	}
 
-	req, _ := http.NewRequest("POST", url, bytes.NewBuffer(v))
-
+	req, _ := http.NewRequest("POST", url+createRoute, bytes.NewBuffer(v))
 	req.Header.Set("Content-Type", "application/octet-stream")
 	client := &http.Client{}
 	client.Do(req)
 
-	fmt.Println("Body:", req.Body)
+	req2, _ := http.NewRequest("GET", url+"/getUsers", nil)
+
+	req2.Header.Set("Content-Type", "application/octet-stream")
+
+	client.Do(req2)
+
+
+	fmt.Printf("Este es el responsecode %d", req2.Response.StatusCode )
+
+
+    fmt.Printf("ESTE ES EL USUARIO %s",req2.Body)
 }
